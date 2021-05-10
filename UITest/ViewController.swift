200cba31daa2532c81e82b5607926acf9a62b142
mraikundalia <71151259+mraikundalia@users.noreply.github.com>
2021-05-10 22:21:10 +0530

    ghgfh
    ghjhg

--- UITest/ViewController.swift
+++ UITest/ViewController.swift
@@ -9,17 +9,14 @@ import UIKit
 
 class ViewController: UIViewController
 {
-    
     @IBOutlet weak var nameTF : UITextField!
-
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
         self.nameTF.text = "m not nil"
     }
-
-
+    
 }
 
